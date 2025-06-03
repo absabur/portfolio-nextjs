7b60ab208599764814ab5d6606e11a45a106f98a
absabur <absabur929@gmail.com>
2025-06-04 01:29:10 +0600

    remove unnecery starback

--- src/Components/Body/Contact/Contact.jsx
+++ src/Components/Body/Contact/Contact.jsx
@@ -36,11 +36,11 @@ const Contact = () => {
           </div>
           <section className="mapbox">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d226.2587106361823!2d89.34657135452704!3d24.859089115907192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fc5500749cdd31%3A0x1432b3a889eaadcb!2sMd%20Abdus%20Sabur!5e0!3m2!1sen!2sbd!4v1744620770614!5m2!1sen!2sbd"
+              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d226.25881877836196!2d89.34647432342385!3d24.85903000947636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sbd!4v1748978327314!5m2!1sen!2sbd"
               width="100%"
               height="100%"
               loading="lazy"
-            />
+            ></iframe>
           </section>
         </div>
         <Form />
