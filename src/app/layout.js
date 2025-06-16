1ce30bcb30b84d33b45572d1158ff9f7aea82aa0
absabur <absabur929@gmail.com>
2025-06-16 23:44:03 +0600

    some meta tag added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,31 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Md Abdus Sabur",
-  description: "Md Abdus Sabur - Full-Stack Web Developer | Expert in JavaScript (MERN Stack) & Python (Django). Specializing in front-end and back-end development with React.js, Next.js, Redux, Django, MongoDB, MySQL, PostgreSQL, SQLite, Selenium, REST APIs, and Django REST Framework. Currently pursuing a BSc in Mathematics and a diploma in Computer Science and Technology. Explore my portfolio for dynamic and robust web solutions.",
+  title: "Md Abdus Sabur | Web Scraping Expert – MERN & Django Developer",
+  description:
+    "Md Abdus Sabur - Full-Stack Web Developer | Expert in JavaScript (MERN Stack) & Python (Django). Specializing in front-end and back-end development with React.js, Next.js, Redux, Django, MongoDB, MySQL, PostgreSQL, SQLite, Selenium, REST APIs, and Django REST Framework. Currently pursuing a BSc in Mathematics and a diploma in Computer Science and Technology. Explore my portfolio for dynamic and robust web solutions.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="google-site-verification" content="KiBk1Z83sel9tb5uLRVxC8AQoL-Y5eTn7QmJ8ZU5tIM" />
+        <meta
+          name="google-site-verification"
+          content="KiBk1Z83sel9tb5uLRVxC8AQoL-Y5eTn7QmJ8ZU5tIM"
+        />
+        <meta name="generator" content="Next.js" />
+        <meta name="author" content="Md Abdus Sabur" />
+        <meta
+          property="og:title"
+          content="Md Abdus Sabur – Automation & Scraping Expert"
+        />
+        <meta
+          property="og:description"
+          content="Selenium, Beautiful Soup, and full-stack expertise in web scraping and automation."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Md Abdus Sabur" />
       </head>
       <body>{children}</body>
     </html>
