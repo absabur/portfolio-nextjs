adadd381fdcc9ca6293f4e22ec0c8629c9d5a236
Md Abdus Sabur <absabur929@gmail.com>
2025-12-17 15:14:01 +0600

    location map updated

--- src/Components/Body/Contact/Contact.jsx
+++ src/Components/Body/Contact/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
           </div>
           <section className="mapbox">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d228.24624951458637!2d90.3497049006094!3d23.749519203681558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1761366035997!5m2!1sen!2sbd"
+              src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d226.25883696258595!2d89.34656826830731!3d24.859020070653425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1sen!2sbd!4v1765961607464!5m2!1sen!2sbd"
               width="100%"
               height="100%"
               loading="lazy"
